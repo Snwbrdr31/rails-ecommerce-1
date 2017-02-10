dcc676979254a9d70fd3b2db40523ceb4b95360b
Ian Wright <wright.ianb@gmail.com>
2017-02-10 15:57:39 -0700

    small typos

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   #has_attached_file :image, styles: {medium: "300x300>", thumb: "100x100>"}
 
   validates_presence_of :name
-  # TODO: add vaildator for pricing format
+  # TODO: add validator for pricing format
   validates_presence_of :price
   validates_presence_of :description
 
