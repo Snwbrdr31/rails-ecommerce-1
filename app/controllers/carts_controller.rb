dbdf156078d403e6b16c66ae7ec4295b16012776
Ian Wright <wright.ianb@gmail.com>
2017-02-13 15:36:02 -0700

    refacctors cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,29 +1,11 @@
 class CartsController < ApplicationController
 
-  before_action :set_cart, only: [:show, :update, :destroy]
+  before_action :set_cart
 
   def show
   end
 
-  def create
-    @cart = Cart.new(cart_params)  
-  end
-
-  def update
-    @cart = Cart.new(cart_params)
-    @cart.save
-    redirect_to cart_path(@cart)
-  end
-
-  def destroy
-    @cart.destroy
-  end
-
   private
-    def cart_params
-      params.require(:cart)
-    end
-
     def set_cart
       @cart = Cart.find(params[:id])
     end
