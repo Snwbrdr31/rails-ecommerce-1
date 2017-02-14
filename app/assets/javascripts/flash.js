cdb57641f98308ae7e1e533968b4ad852ed40529
Ian Wright <wright.ianb@gmail.com>
2017-02-13 21:12:00 -0700

    adjusts styles

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
-  function addFlash(type, msg) {
-    var flash = '<div class="alert alert-' + type + '">' + msg + '</div>'
-    $('nav').after(flash);
-    removeFlash();
-  }
 
-  function removeFlash(clicked) {
-    if (clicked) {
-      $('.alert').slideUp('slow', function() {
-        $('.alert').remove();
-      });
-    } else {
-      setTimeout( function() {
-        $('.alert').slideUp('slow', function() {
-          $('.alert').remove();
-        });
-      }, 3000);
-    }
-  }
+  $(".button-collapse").sideNav();
+
+  setTimeout( function() {
+    $('.alert').slideUp('slow', function() {
+      $('.alert').remove();
+    });
+  }, 3000);
 
   $(document).on('click', '.alert', function() {
-    removeFlash(true);
+    $('.alert').slideUp('slow', function() {
+      $('.alert').remove();
+    });
   });
+
 });
